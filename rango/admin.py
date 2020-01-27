c26f82ebb046f43e5ae3ec075d5c208e6a940d94
ineshbose <ineshbose@yahoo.com>
2020-01-27 15:10:37 +0000

    Chapter 6 done, on Ex 6 now.

--- rango/admin.py
+++ rango/admin.py
@@ -3,8 +3,11 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'url']
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    #context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
     #return HttpResponse("Rango says hey there partner! Go to <a href='/rango/about/'>About</a>")
 
@@ -10,3 +16,15 @@ def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Inesh Bose.'}
     return render(request, 'rango/about.html', context=context_dict)
     #return HttpResponse("Rango says here is the about page. Go to <a href='/rango/'>Index</a>")
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['cateogry'] = None
+    return render(request, 'rango/category.html', context=context_dict)
