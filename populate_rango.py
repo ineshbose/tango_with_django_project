1f9ff8b1d2aba64d1b2b3db3ef18df074135c1c3
ineshbose <ineshbose@yahoo.com>
2020-01-28 22:28:21 +0000

    Little details fixed after tallying with progress_tests

--- populate_rango.py
+++ populate_rango.py
@@ -54,12 +54,14 @@ def populate():
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
-    p.views=p.views + views
+    p.views=views
     p.save()
     return p
 
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
