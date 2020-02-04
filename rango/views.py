eb6c19b5f1268e04a417950dc881978b36c91d40
ineshbose <ineshbose@yahoo.com>
2020-02-04 18:14:10 +0000

    Ex 9 done

--- rango/views.py
+++ rango/views.py
@@ -38,6 +38,7 @@ def show_category(request, category_name_slug):
         context_dict['cateogry'] = None
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -49,6 +50,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -115,7 +117,8 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
+    #return HttpResponse("Since you're logged in, you can see this text!")
 
 @login_required
 def user_logout(request):
