9b04c225849c054fb6b951940e3c8957474642f5
ineshbose <ineshbose@yahoo.com>
2020-02-04 18:28:50 +0000

    Ex 9 passed after removing password validator

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS' : { 'min_length': 6, }
+        #'OPTIONS' : { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
