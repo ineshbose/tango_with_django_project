94be246bffc30a14d7d1017e7c960ffb21bad24c
ineshbose <ineshbose@yahoo.com>
2020-02-12 00:35:55 +0000

    I hope this is the last commit.

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'url']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
